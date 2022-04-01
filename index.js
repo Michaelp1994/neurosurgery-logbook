0223b15e58603e6d2230ec29b90c7d755573ca22
Michael Poulgrain <michael.poulgrain@gmail.com>
2022-04-01 20:32:07 +0200

    Rearranging files

--- index.js
+++
@@ -1,6 +0,0 @@
-const startServer = require("./packages/server/dist/index.js");
-const clientFiles = __dirname + "/packages/web/dist";
-startServer.default(clientFiles).catch((e) => {
-  console.log("Error:", e);
-});
-// startServer();
