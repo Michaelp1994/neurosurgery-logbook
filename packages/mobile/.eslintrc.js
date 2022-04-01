b101fe56b4dba782b52561b4d71688fbf8d31a7f
Michael Poulgrain <michael.poulgrain@gmail.com>
2022-04-01 22:56:44 +0200

    build(moved mobile package around. ): moved mobile
    making changes to the monorepo structure and in the process the mobile

---
+++ packages/mobile/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    "vue/setup-compiler-macros": true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+  ],
+  ignorePatterns: ["**/*.generated.ts"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  plugins: ["vue"],
+  rules: {
+    "vue/component-tags-order": [
+      "error",
+      {
+        order: ["script", "template", "style"],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.graphql"],
+      extends: "plugin:@graphql-eslint/operations-recommended",
+      rules: {
+        "prettier/prettier": "error",
+        // "@graphql-eslint/match-document-filename": 0,
+      },
+    },
+  ],
+};
